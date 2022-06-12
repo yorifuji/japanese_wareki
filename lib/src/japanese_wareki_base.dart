90ec5b57030ac83f99c401cd34a181431697d20c
yorifuji <yorifuji@gmail.com>
2022-06-12 21:37:18 +0900

    Implement

--- lib/src/japanese_wareki_base.dart
+++ lib/src/japanese_wareki_base.dart
@@ -1,6 +1,37 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+String wareki(int year, int month, int day) {
+  String name, index;
+  if (year > 2019) {
+    name = "令和";
+    index = '${year - 2018}年';
+  } else if (year == 2019 && month >= 5) {
+    name = "令和";
+    index = "元年";
+  } else if (year > 1989) {
+    name = "平成";
+    index = '${year - 1988}年';
+  } else if (year == 1989 && (month > 1 || (month == 1 && day >= 8))) {
+    name = "平成";
+    index = "元年";
+  } else if (year > 1926) {
+    name = "昭和";
+    index = '${year - 1925}年';
+  } else if (year == 1926 && (month == 12 && day >= 25)) {
+    name = "昭和";
+    index = "元年";
+  } else if (year > 1912) {
+    name = "大正";
+    index = '${year - 1911}年';
+  } else if (year == 1912 && (month > 7 || (month == 7 && day >= 30))) {
+    name = "大正";
+    index = "元年";
+  } else if (year > 1868) {
+    name = "明治";
+    index = '${year - 1867}年';
+  } else if (year == 1868 && (month > 1 || (month == 1 && day >= 25))) {
+    name = "明治";
+    index = "元年";
+  } else {
+    throw Exception();
+  }
+  return name + index;
 }
--- test/japanese_wareki_test.dart
+++ test/japanese_wareki_test.dart
@@ -2,15 +2,23 @@ import 'package:japanese_wareki/japanese_wareki.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+  group('Group', () {
+    test('Test', () {
+      expect(wareki(2020, 1, 1), '令和2年');
+      expect(wareki(2019, 5, 1), '令和元年');
+      expect(wareki(2019, 4, 30), '平成31年');
+      expect(wareki(1990, 1, 1), '平成2年');
+      expect(wareki(1989, 1, 8), '平成元年');
+      expect(wareki(1989, 1, 7), '昭和64年');
+      expect(wareki(1927, 1, 1), '昭和2年');
+      expect(wareki(1926, 12, 25), '昭和元年');
+      expect(wareki(1926, 12, 24), '大正15年');
+      expect(wareki(1913, 1, 1), '大正2年');
+      expect(wareki(1912, 7, 30), '大正元年');
+      expect(wareki(1912, 7, 29), '明治45年');
+      expect(wareki(1869, 1, 1), '明治2年');
+      expect(wareki(1868, 1, 25), '明治元年');
+      expect(() => wareki(1868, 1, 24), throwsException);
     });
   });
 }
